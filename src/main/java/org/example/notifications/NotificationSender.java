0a3273bed4ebe452c38dc62adfedaaaf76830bb6
taivs93 <taivs93@gmail.com>
2025-06-02 01:56:52 +0700

    update

--- src/main/java/org/example/notifications/NotificationSender.java
+++ src/main/java/org/example/notifications/NotificationSender.java
@@ -10,40 +10,13 @@ import javax.annotation.PreDestroy;
 @Component
 public class NotificationSender {
 
-
     private NotificationService notificationService;
-
-    private NotificationService smsmNotificationService;
-
-    private NotificationService emailNotification;
-
-    private NotificationService pushNotification;
 
     @Autowired
     public void setNotificationService(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
 
-    @Autowired
-    @Qualifier("smsNotification")
-    public void setSmsNotificationService(NotificationService smsmNotificationService) {
-        this.smsmNotificationService = smsmNotificationService;
-        this.notificationService = smsmNotificationService;
-    }
-
-    @Autowired
-    @Qualifier("emailNotification")
-    public void setEmailNotification(NotificationService emailNotification) {
-        this.emailNotification = emailNotification;
-        this.notificationService = emailNotification;
-    }
-
-    @Autowired
-    @Qualifier("pushNotification")
-    public void setPushNotification(NotificationService pushNotification) {
-        this.pushNotification = pushNotification;
-        this.notificationService = pushNotification;
-    }
 
     @PostConstruct
     public void init() {
