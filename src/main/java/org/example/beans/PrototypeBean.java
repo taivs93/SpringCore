7a03808ac6f6f7ae035924ad8aa98fd5645f9ac4
taivs93 <taivs93@gmail.com>
2025-06-02 12:18:45 +0700

    update

--- src/main/java/org/example/beans/PrototypeBean.java
+++ src/main/java/org/example/beans/PrototypeBean.java
@@ -1,13 +1,15 @@
 package org.example.beans;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Component
+@Scope("prototype")
 public class PrototypeBean {
-    private int count = 0;
+    private static int count = 0;
 
     @PostConstruct
     public void init() {
